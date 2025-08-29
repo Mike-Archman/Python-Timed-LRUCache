de6febf484b8182dab604ad815a3ae93d9412eff
Mike Archman <mikearchman.01@gmail.com>
2025-08-29 15:39:49 +0300

    Implemented the cache system.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Python Timed LRU Cache
+
+This implements a lru cache system that has time-to-live control option which in turn can be controlled at per value level.
+
+Example Usage:
+```
+from timed_lru_caching import timed_lru_cache
+
+THE_SET_TTL = 10 * 60 # seconds
+
+def fetch_from_cache(*args, **kwargs):
+    data, save_ts = criterion_to_get_cached_data()
+    if time.time() - save_ts < THE_SET_TTL:
+        return cached_data, save_ts
+    else:
+        ...
+        return None
+
+def save_to_cache(data, save_ts):
+    ...
+
+@timed_lru_cache(max_size=5, ttl=THE_SET_TTL)
+def fetch_from_api(*args, **kwargs):
+    from_cache = fetch_from_cache(...)
+    if not from_cache:
+        data = requests.get(...).json()
+        save_ts = time.time()
+        save_to_cache(data, save_ts)
+    else:
+        data, save_ts = from_cache
+    return data, save_ts
+```
+
+So this could be used in cases where the caching is to be maintained across program runs.
---
+++ timed_lru_caching.py
@@ -0,0 +1,187 @@
+from collections import OrderedDict
+from collections.abc import Callable, Hashable
+import time
+from typing import NamedTuple
+import functools
+
+# =============================================================================================
+
+class _TimedLRUCacheItem[T](NamedTuple):
+    """Hold the timed value in TimedLRUCache"""
+
+    value: T
+    time_added: float
+
+# =============================================================================================
+
+class TimedLRUCache[T]:
+    """Class to create a LRU cache object that has time-to-live control."""
+
+    def __init__(self, max_size: int = None, ttl: float = None):
+        """Create a LRU cache object with time-to-live control.
+
+        Args:
+            max_size (int):
+                The maximum items the cache should hold after which least used
+                items are evicted.
+
+                When null, the cache has limitless capacity.
+            ttl (float):
+                The time in seconds representing how long each item should
+                live in cache after which they are considered stale and are
+                evicted from cache.
+
+                When null, the items in cache live forever unless evicted on
+                basis of cache capacity limit, if any is set.
+        """
+
+        self._max_size = max_size
+        self._ttl = ttl
+        self._cache: OrderedDict[Hashable, _TimedLRUCacheItem[T]] = OrderedDict()
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def get(self, key: Hashable) -> T | None:
+        """Return value from cache under the key if any else return None."""
+
+        ct = time.time()
+
+        if key not in self._cache:
+            return None
+
+        item = self._cache[key]
+
+        # check if expired
+        if self._ttl and ct - item.time_added >= self._ttl:
+            # remove and return None
+            self._cache.pop(key)
+            return None
+
+        # move key to end and return item
+        self._cache.move_to_end(key)
+
+        return item.value
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def add(self, key: Hashable, value: T, time_added: float | None = None) -> None:
+        """Add or update value in cache under the key."""
+
+        ct = time.time() if time_added is None else time_added
+
+        # cap reached, evict least used item
+        if self._max_size and len(self._cache) == self._max_size:
+            self._cache.popitem(last=False)
+
+        # add the value, timestamp the value
+        self._cache[key] = _TimedLRUCacheItem(value, ct)
+
+        # move the new key up
+        self._cache.move_to_end(key)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def __len__(self) -> int:
+        """Return the number of values in cache."""
+
+        return len(self._cache)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def clear(self) -> None:
+        """Remove all values from cache."""
+
+        self._cache.clear()
+
+# =============================================================================================
+
+class _TimedLRUCacheInfo(NamedTuple):
+    """Holds info about current state of the target timed lru cache."""
+
+    hits: int
+    misses: int
+    current_size: int
+    max_size: int
+
+# =============================================================================================
+
+class _TimedLRUCacheWrapper[**P, T]:
+    """Wrapper to apply timed lru cache on a function."""
+
+    def __init__(
+        self, fn: Callable[P, tuple[T, float | None]], max_size: int = None, ttl: float = None
+    ):
+        """Return a wrapper that applies a timed lru cache on the target func."""
+
+        self._wrapped = fn
+        self._time_lru_cache = TimedLRUCache[T](max_size=max_size, ttl=ttl)
+        self._hits = 0
+        self._misses = 0
+        self._max_size = max_size
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        """"""
+
+        call_args = args + tuple(kwargs.items())
+
+        res = self._time_lru_cache.get(call_args)
+
+        if res is None:
+            self._misses += 1
+            res, add_ts = self._wrapped(*args, **kwargs)
+            self._time_lru_cache.add(call_args, res, time_added=add_ts)
+        else:
+            self._hits += 1
+
+        return res
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def cache_info(self) -> _TimedLRUCacheInfo:
+        """Return current state of the timed lru cache."""
+
+        return _TimedLRUCacheInfo(
+            self._hits, self._misses, len(self._time_lru_cache), self._max_size
+        )
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def clear(self) -> None:
+        """Clear the cache of the wrapped function."""
+
+        self._time_lru_cache.clear()
+        self._hits = 0
+        self._misses = 0
+
+# =============================================================================================
+
+def timed_lru_cache[**P, T](
+    max_size: int = None, ttl: float = None
+) -> Callable[[Callable[P, tuple[T, float | None]]], _TimedLRUCacheWrapper[P, T]]:
+    """A decorator that applies a timed lru cache on the function.
+
+    The function to wrap must return a tuple. The first value being the value
+    computed by the function and second value being an optional timestamp. This
+    allows for per value time-to-live (ttl) control. A good use for this would
+    be to sync cached data across disk and memory.
+
+    Args:
+        max_size (int):
+            The maximum number of values the cache can hold.
+
+            When null, the cache can grow without limit.
+        ttl (float):
+            Time is seconds after which the values in cache are considered
+            stale.
+
+            When null, the values in cache live forever unless evicted on the
+            basis of cache capacity control.
+    """
+
+    def deco(fn: Callable[P, tuple[T, float | None]]) -> _TimedLRUCacheWrapper[P, T]:
+        return _TimedLRUCacheWrapper(fn, max_size=max_size, ttl=ttl)
+    return deco
+
+# =============================================================================================
